b4129d781328be318f4b7c9bf843a91b1500a869
Ross Wintle <ross@oikos.org.uk>
2015-07-29 20:51:13 +0100

    Initial commit based on Laravel core. I think I tweaked the routes.php for subdomains.

---
+++ app/Http/routes.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::group( ['domain' => '{subdomain}.localhost'], function () {
+    Route::get('/', function($subdomain) {
+        return $subdomain;
+    });
+} );
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
