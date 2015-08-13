ccdda1fd4acd1bdaad3510e61e473694edb33a71
Ross Wintle <ross@oikos.org.uk>
2015-08-13 23:20:10 +0100

    Added auth and password reset

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,31 @@
 |
 */
 
+// Routes with subdomains
 Route::group( ['domain' => '{subdomain}.' . env('DOMAIN')], function () {
     Route::get('/', 'User@profile');
     Route::get('profile', 'User@profile');
 } );
 
+// Profile route with no subdomain
+Route::get('profile', 'User@profile');
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+// Password reset link request routes...
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset routes...
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
 
 Route::get('/', function () {
     return view('welcome');
