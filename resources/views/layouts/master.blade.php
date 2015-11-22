2087e5de27329d1b338b711cdc6496f58c935cdf
Ross Wintle <ross@oikos.org.uk>
2015-11-22 21:14:11 +0000

    Footer: Add basic footer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -88,6 +88,10 @@
             @yield('content')
         </div>
 
+        <footer>
+
+        </footer>
+
         <!-- Twitter API -->
         <?php /*
             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
