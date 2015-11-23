07dc819b59d8c2ec2ca76570690a62862b2c4aa9
Ross Wintle <ross@oikos.org.uk>
2015-11-23 14:11:09 +0000

    Profiles: Remove references to "yours" on profiles

--- resources/views/your-profile.blade.php
+++ resources/views/your-profile.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.master')
 
-@section('display_name', $profile->display_name . ' (yours)')
 @section('title', $profile->display_name . ' - has your baby arrived yet? - hasyourbabyarrivedyet.com')
 
 @section('description', $profile->display_name . ' - has your baby arrived yet? Find out on their simple birth announcement page at hasyourbabyarrivedyet.com')
 
+@section('display_name', $profile->display_name)
 
 @section('content')
     <div class="row">
         <div class="col-xs-12 text-center">
-            <h1 class="display-name">{{ $profile->display_name }} (yours)</h1>
+            <h1 class="display-name">{{ $profile->display_name }}</h1>
             <h2>Current status:</h2>
             <div class="small-answer">{{ $profile->statusAsString() }}</div>
             <h2>Change status:</h2>
