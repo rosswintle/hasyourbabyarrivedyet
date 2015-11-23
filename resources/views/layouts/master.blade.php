e11016af1c3d597dc713c9198dba18e1def92eab
Ross Wintle <ross@oikos.org.uk>
2015-11-23 14:08:57 +0000

    Meta: Add favicons

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,22 +8,22 @@
         <meta name="MobileOptimized" content="320">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-        <link rel="apple-touch-icon" sizes="57x57" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/apple-touch-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/apple-touch-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/apple-touch-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/apple-touch-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/apple-touch-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/apple-touch-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/apple-touch-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/apple-touch-icon-180x180.png">
-        <link rel="icon" type="image/png" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/favicon-192x192.png" sizes="192x192">
-        <link rel="icon" type="image/png" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/favicon-160x160.png" sizes="160x160">
-        <link rel="icon" type="image/png" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/favicon-96x96.png" sizes="96x96">
-        <link rel="icon" type="image/png" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/favicon-16x16.png" sizes="16x16">
-        <link rel="icon" type="image/png" href="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/favicon-32x32.png" sizes="32x32">
+        <link rel="apple-touch-icon" sizes="57x57" href="http://{{ env('DOMAIN') }}/images/favicons/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="http://{{ env('DOMAIN') }}/images/favicons/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="http://{{ env('DOMAIN') }}/images/favicons/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="http://{{ env('DOMAIN') }}/images/favicons/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="http://{{ env('DOMAIN') }}/images/favicons/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="http://{{ env('DOMAIN') }}/images/favicons/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="http://{{ env('DOMAIN') }}/images/favicons/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="http://{{ env('DOMAIN') }}/images/favicons/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="http://{{ env('DOMAIN') }}/images/favicons/apple-touch-icon-180x180.png">
+        <link rel="icon" type="image/png" href="http://{{ env('DOMAIN') }}/images/favicons/favicon-192x192.png" sizes="192x192">
+        <link rel="icon" type="image/png" href="http://{{ env('DOMAIN') }}/images/favicons/favicon-160x160.png" sizes="160x160">
+        <link rel="icon" type="image/png" href="http://{{ env('DOMAIN') }}/images/favicons/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="http://{{ env('DOMAIN') }}/images/favicons/favicon-16x16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="http://{{ env('DOMAIN') }}/images/favicons/favicon-32x32.png" sizes="32x32">
         <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="msapplication-TileImage" content="http://hasyourbabyarrivedyet.com/wp-content/themes/hasyourbabyarrivedyet/favicons/mstile-144x144.png">
+        <meta name="msapplication-TileImage" content="http://{{ env('DOMAIN') }}/images/faviconsmstile-144x144.png">
 
         <link rel='stylesheet' id='googleFonts-css'  href='http://fonts.googleapis.com/css?family=Lato%3A400%2C700%2C400italic%2C700italic%7CLife+Savers%7CRanchers%7CVarela+Round' type='text/css' media='all' />
         <script src="{{ elixir('js/all.js') }}" type="text/javascript"></script>
