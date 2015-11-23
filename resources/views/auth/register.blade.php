0e7230ee85135e6c447cda54aecbd4d53b08e2b2
Ross Wintle <ross@oikos.org.uk>
2015-11-23 21:04:54 +0000

    Registration: Add form field explanations

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,9 +12,13 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Register</div>
                     <div class="panel-body">
+                        <p class="text-center">
+                            To get your page you'll just need to give us a few details and make
+                            some simple choices.
+                        </p>
                         @if (count($errors) > 0)
                             <div class="alert alert-danger">
-                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                <strong>Whoops!</strong> There were some problems with your details.<br><br>
                                 <ul>
                                     @foreach ($errors->all() as $error)
                                         <li>{{ $error }}</li>
@@ -30,6 +34,7 @@
                                 <label class="col-md-4 control-label">Name</label>
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                                    <p class="field-description">Please tell us your name</p>
                                 </div>
                             </div>
 
@@ -37,6 +42,7 @@
                                 <label class="col-md-4 control-label">E-Mail Address</label>
                                 <div class="col-md-6">
                                     <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                    <p class="field-description">Please tell us your email address - you will use this to log in</p>
                                 </div>
                             </div>
 
@@ -44,6 +50,8 @@
                                 <label class="col-md-4 control-label">Display name</label>
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="display_name" value="{{ old('display_name') }}">
+                                    <p class="field-description">The display name will be the name(s) shown on your profile page.
+                                    This will usually be something like "Kate and William"</p>
                                 </div>
                             </div>
 
@@ -51,6 +59,9 @@
                                 <label class="col-md-4 control-label">Domain</label>
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="domain" value="{{ old('domain') }}">
+                                    <p class="field-description">The domain is the bit of the website address that goes
+                                        before "hasyourbabyarrivedyet.com". It must be letters, numbers and hyphens with
+                                    no spaces allowed. e.g. "kateandwilliam"</p>
                                 </div>
                             </div>
 
@@ -58,6 +69,7 @@
                                 <label class="col-md-4 control-label">Password</label>
                                 <div class="col-md-6">
                                     <input type="password" class="form-control" name="password">
+                                    <p class="field-description">Please enter a password</p>
                                 </div>
                             </div>
 
@@ -65,6 +77,7 @@
                                 <label class="col-md-4 control-label">Confirm Password</label>
                                 <div class="col-md-6">
                                     <input type="password" class="form-control" name="password_confirmation">
+                                    <p class="field-description">Please enter your password again</p>
                                 </div>
                             </div>
 
@@ -81,6 +94,7 @@
                                         <option value="dark-green">Dark Green</option>
                                         <option value="mint-green">Mint Green</option>
                                     </select>
+                                    <p class="field-description">Choose a colour-scheme for your page</p>
                                 </div>
                             </div>
 
@@ -88,6 +102,8 @@
                                 <label class="col-md-4 control-label"></label>
                                 <div class="col-md-6">
                                     {!! Recaptcha::render() !!}
+                                    <p class="field-description">Unfortunately we need you to do this for security and to
+                                        prevent spam registrations.</p>
                                 </div>
                             </div>
 
