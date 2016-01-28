0a63c51e4b4073e52364984eba3607eb0d32652e
Ross Wintle <ross@oikos.org.uk>
2016-01-28 21:57:00 +0000

    Analytics: Google Analytics is now conditional on an environment variable being present.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -117,27 +117,29 @@
             </div>
         </footer>
 
-        <!-- Google Analytics -->
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        @if (env('GOOGLE_ANALYTICS_ID'))
+            <!-- Google Analytics -->
+            <script>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            ga('create', 'UA-36775788-1', 'auto');
-            ga('send', 'pageview');
+                ga('create', '{{ env('GOOGLE_ANALYTICS_ID') }}', 'auto');
+                ga('send', 'pageview');
 
-            @if (isset($send_ga_event))
-                ga('send', {
-                    hitType: 'event',
-                    eventCategory: '{{ $send_ga_event['category'] }}',
-                    eventAction: '{{ $send_ga_event['action'] }}',
-                    eventLabel: '{{ $send_ga_event['label'] }}',
-                    eventValue: '{{ $send_ga_event['value'] }}'
-                });
-            @endif
+                @if (isset($send_ga_event))
+                    ga('send', {
+                        hitType: 'event',
+                        eventCategory: '{{ $send_ga_event['category'] }}',
+                        eventAction: '{{ $send_ga_event['action'] }}',
+                        eventLabel: '{{ $send_ga_event['label'] }}',
+                        eventValue: '{{ $send_ga_event['value'] }}'
+                    });
+                @endif
 
-        </script>
+            </script>
+        @endif
         <!-- Twitter API -->
         <?php /*
             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
